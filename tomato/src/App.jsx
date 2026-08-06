4df159344134d08dbdd89f73875b0fa43d270969
gsacenter <gsacenter810@gmail.com>
2026-08-06 16:33:48 +0530

    My first project

---
+++ tomato/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home/Home.jsx"
+import Cart from "./pages/Cart/Cart.jsx"
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+
+const App = () => {
+  return (
+    <div className='app'>
+       <Navbar/>
+       <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+       </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ tomato/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>choose form a diverse menu featuring a delectable arry of dishes crafted with the finest ingredients and culinary expertise, Our mission is to satisfy your cravings and elevate your dining expenses. one delicious meal at a time</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
